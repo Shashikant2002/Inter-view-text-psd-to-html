2a5fe891d32eb972341cb109f16481a47dfa03a8
Shashikant <shashikantprajapti17@gmail.com>
2022-11-18 14:10:27 +0530

    some

--- js/script.js
+++ js/script.js
@@ -1,6 +1,54 @@
+AOS.init();
+
 const open_close_bar = () => {
   const bars = document.getElementById("bars");
-  const navbar = document.getElementById('nav_bar');
+  const navbar = document.getElementById("nav_bar");
   bars.classList.toggle("clicked");
   navbar.classList.toggle("show_nav");
 };
+
+var a = 0;
+$(window).scroll(function () {
+  var oTop = $("#counter-box").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $(".counter").each(function () {
+      var $this = $(this),
+        countTo = $this.attr("data-number");
+      $({
+        countNum: $this.text(),
+      }).animate(
+        {
+          countNum: countTo,
+        },
+        {
+          duration: 850,
+          easing: "swing",
+          step: function () {
+            $this.text(Math.ceil(this.countNum).toLocaleString("en"));
+          },
+          complete: function () {
+            $this.text(Math.ceil(this.countNum).toLocaleString("en"));
+          },
+        }
+      );
+    });
+    a = 1;
+  }
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
